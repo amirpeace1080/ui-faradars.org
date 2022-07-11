63ada69b2b151c840fcf67a9ed5fd8819a2c8170
amirpeace1080 <amirpeace1080@gmail.com>
2022-07-11 17:41:49 +0430

    end

--- src/components/RouteLearn.vue
+++ src/components/RouteLearn.vue
@@ -1,18 +1,268 @@
 <template>
   <div>
     <v-img width="100%" src="https://faradars.org/next/svg/roadMap.svg" />
-    <div style="height: 945px; background-color: #181818">
+    <div style="height: auto; background-color: #181818">
+      <v-container>
+        <p class="white--text text-center display-1" style="margin-top: 100px">
+          مسیرهای یادگیری و مجموعه‌های آموزشی
+        </p>
+        <p class="white--text text-center subtitle-1">
+          هدف خود را انتخاب کنید و با مجموعه‌های آموزشی زیر به آن برسید.
+        </p>
 
+        <!-- start items -->
+
+        <v-container class="mt-5">
+          <v-row>
+
+            <v-col cols="12" lg="6" class="mt-2">
+              <v-card color="#e5e7eb" height="88" width="530">
+                <div class="d-flex flex-row">
+                  <div class="d-flex align-center mr-4 mt-3" style="width: 120px">
+                    <v-img
+                      height="65"
+                      width="113"
+                      src="https://faradars.org/wp-content/uploads/2018/01/programming.png.png"
+                    />
+                  </div>
+
+                  <div style="width: 90%">
+                    <p class="mr-2 mt-3 font-weight-bold subtitle-2">برنامه نویسی</p>
+                    <div class="d-flex justify-space-around">
+                        <span>۶۸ ‌آموزش</span>
+                        <span>۵۳۲ ساعت</span>
+                        <span>۳۱۴,۲۹۲ دانشجو</span>
+                    </div>
+                  </div>
+                </div>
+              </v-card>
+            </v-col>
+
+
+            <v-col cols="12" lg="6" class="mt-2">
+              <v-card color="#e5e7eb" height="88" width="530">
+                <div class="d-flex flex-row">
+                  <div class="d-flex align-center mr-4 mt-3" style="width: 120px">
+                    <v-img
+                      height="65"
+                      width="113"
+                      src="https://faradars.org/wp-content/uploads/2021/01/16/burse-png.png"
+                    />
+                  </div>
+
+                  <div style="width: 90%">
+                    <p class="mr-2 mt-3 font-weight-bold subtitle-2">آموزش های بورس و تحلیل تکنیکال</p>
+                    <div class="d-flex justify-space-around">
+                        <span>۶۱ ‌آموزش</span>
+                        <span>۳۰۳ ساعت</span>
+                        <span>۱۹۰,۴۰۶ دانشجو</span>
+                    </div>
+                  </div>
+                </div>
+              </v-card>
+            </v-col>
+
+            <v-col cols="12" lg="6" class="mt-2">
+              <v-card color="#e5e7eb" height="88" width="530">
+                <div class="d-flex flex-row">
+                  <div class="d-flex align-center mr-4 mt-3" style="width: 120px">
+                    <v-img
+                      height="65"
+                      width="113"
+                      src="https://faradars.org/wp-content/uploads/2018/07/general.svg"
+                    />
+                  </div>
+
+                  <div style="width: 90%">
+                    <p class="mr-2 mt-3 font-weight-bold subtitle-2">آموزش های عمومی</p>
+                    <div class="d-flex justify-space-around">
+                        <span>۸۶ ‌آموزش</span>
+                        <span>۲۸۶ ساعت</span>
+                        <span>۱۸۹,۲۷۴ دانشجو</span>
+                    </div>
+                  </div>
+                </div>
+              </v-card>
+            </v-col>
+
+            <v-col cols="12" lg="6" class="mt-2">
+              <v-card color="#e5e7eb" height="88" width="530">
+                <div class="d-flex flex-row">
+                  <div class="d-flex align-center mr-4 mt-3" style="width: 120px">
+                    <v-img
+                      height="65"
+                      width="113"
+                      src="https://faradars.org/wp-content/uploads/2018/10/02.svg"
+                    />
+                  </div>
+
+                  <div style="width: 90%">
+                    <p class="mr-2 mt-3 font-weight-bold subtitle-2">آموزش نرم افزار های Adobe</p>
+                    <div class="d-flex justify-space-around">
+                        <span>۵۳ ‌آموزش</span>
+                        <span>۲۵۳ ساعت</span>
+                        <span>۱۷۷,۲۱۸ دانشجو</span>
+                    </div>
+                  </div>
+                </div>
+              </v-card>
+            </v-col>
+
+            <v-col cols="12" lg="6" class="mt-2">
+              <v-card color="#e5e7eb" height="88" width="530">
+                <div class="d-flex flex-row">
+                  <div class="d-flex align-center mr-4 mt-3" style="width: 120px">
+                    <v-img
+                      height="65"
+                      width="113"
+                      src="https://faradars.org/wp-content/uploads/2015/11/icdl.png.png"
+                    />
+                  </div>
+
+                  <div style="width: 90%">
+                    <p class="mr-2 mt-3 font-weight-bold subtitle-2">مهارت های کاربردی کامپیوتر ICDL</p>
+                    <div class="d-flex justify-space-around">
+                        <span>۲۴ ‌آموزش</span>
+                        <span>۹۴ ساعت</span>
+                        <span>۱۶۹,۲۰۸ دانشجو</span>
+                    </div>
+                  </div>
+                </div>
+              </v-card>
+            </v-col>
+
+             <v-col cols="12" lg="6" class="mt-2">
+              <v-card color="#e5e7eb" height="88" width="530">
+                <div class="d-flex flex-row">
+                  <div class="d-flex align-center mr-4 mt-3" style="width: 120px">
+                    <v-img
+                      height="65"
+                      width="113"
+                      src="https://faradars.org/wp-content/uploads/2016/04/web-design-and-programming.png.png"
+                    />
+                  </div>
+
+                  <div style="width: 90%">
+                    <p class="mr-2 mt-3 font-weight-bold subtitle-2">آموزش های طراحی سایت</p>
+                    <div class="d-flex justify-space-around">
+                        <span>۶۱ ‌آموزش</span>
+                        <span>۳۱۷ ساعت</span>
+                        <span>۱۶۵,۷۹۷ دانشجو</span>
+                    </div>
+                  </div>
+                </div>
+              </v-card>
+            </v-col>
+
+             <v-col cols="12" lg="6" class="mt-2">
+              <v-card color="#e5e7eb" height="88" width="530">
+                <div class="d-flex flex-row">
+                  <div class="d-flex align-center mr-4 mt-3" style="width: 120px">
+                    <v-img
+                      height="65"
+                      width="113"
+                      src="https://faradars.org/wp-content/uploads/2016/09/students-and-teenagers.png.png"
+                    />
+                  </div>
+
+                  <div style="width: 90%">
+                    <p class="mr-2 mt-3 font-weight-bold subtitle-2">آموزش های ویژه نوجوانان</p>
+                    <div class="d-flex justify-space-around">
+                        <span>۲۳ ‌آموزش</span>
+                        <span>۱۱۱ ساعت</span>
+                        <span>۱۵۱,۷۷۷ دانشجو</span>
+                    </div>
+                  </div>
+                </div>
+              </v-card>
+            </v-col>
+
+            <v-col cols="12" lg="6" class="mt-2">
+              <v-card color="#e5e7eb" height="88" width="530">
+                <div class="d-flex flex-row">
+                  <div class="d-flex align-center mr-4 mt-3" style="width: 120px">
+                    <v-img
+                      height="65"
+                      width="113"
+                      src="https://faradars.org/wp-content/uploads/2015/11/microsoft-office.png.png"
+                    />
+                  </div>
+
+                  <div style="width: 90%">
+                    <p class="mr-2 mt-3 font-weight-bold subtitle-2">آموزش های آفیس Microsoft Office</p>
+                    <div class="d-flex justify-space-around">
+                        <span>۲۸ ‌آموزش</span>
+                        <span>۹۷ ساعت</span>
+                        <span>۲۵۵,۹۹۷ دانشجو</span>
+                    </div>
+                  </div>
+                </div>
+              </v-card>
+            </v-col>
+
+            <v-col cols="12" lg="6" class="mt-2">
+              <v-card color="#e5e7eb" height="88" width="530">
+                <div class="d-flex flex-row">
+                  <div class="d-flex align-center mr-4 mt-3" style="width: 120px">
+                    <v-img
+                      height="65"
+                      width="113"
+                      src="https://faradars.org/wp-content/uploads/2015/11/microsoft-excel.png.png"
+                    />
+                  </div>
+
+                  <div style="width: 90%">
+                    <p class="mr-2 mt-3 font-weight-bold subtitle-2">آموزش های اکسل Excel</p>
+                    <div class="d-flex justify-space-around">
+                        <span>۲۱ ‌آموزش</span>
+                        <span>۸۲ ساعت</span>
+                        <span>۱۳۷,۲۵۲ دانشجو</span>
+                    </div>
+                  </div>
+                </div>
+              </v-card>
+            </v-col>
+
+            <v-col cols="12" lg="6" class="mt-2">
+              <v-card color="#e5e7eb" height="88" width="530">
+                <div class="d-flex flex-row">
+                  <div class="d-flex align-center mr-4 mt-3" style="width: 120px">
+                    <v-img
+                      height="65"
+                      width="113"
+                      src="https://faradars.org/wp-content/uploads/2015/11/computer-science.png.png"
+                    />
+                  </div>
+
+                  <div style="width: 90%">
+                    <p class="mr-2 mt-3 font-weight-bold subtitle-2">دروس علوم و مهندسی کامپیوتر</p>
+                    <div class="d-flex justify-space-around">
+                        <span>۴۴ ‌آموزش</span>
+                        <span>۵۱۰ ساعت</span>
+                        <span>۱۲۵,۸۵۵ دانشجو</span>
+                    </div>
+                  </div>
+                </div>
+              </v-card>
+            </v-col>
+
+          </v-row>
+        </v-container>
+
+        <div class="d-flex justify-center mt-5">
+            <v-btn color="primary" class="mt-5">
+            همه آموزش ها
+        </v-btn>
+        </div>
+        <!-- end items -->
+      </v-container>
     </div>
   </div>
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
 
 <style>
-
 </style>
